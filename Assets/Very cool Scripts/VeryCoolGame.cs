6bd38645180e043ff3588e4d5dcf49b1361f4d4f
lac1dly <maksumchorney@gmail.com>
2024-01-10 20:01:16 +0200

    Changed Script-"VeryCoolGame" 10.01.24
    Вивів інформацію в консоль про крутість цієї гри

--- Assets/Very cool Scripts/VeryCoolGame.cs
+++ Assets/Very cool Scripts/VeryCoolGame.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class VeryCoolGame : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("This game is very cool fr");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
